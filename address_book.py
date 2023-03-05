9cd1a3a6d03caf7fc038408326cf748e85ef61ca
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-03-05 08:24:46 -0600

    Add files via upload

--- address_book.py
+++ address_book.py
@@ -1,8 +1,12 @@
-file = open("text.file", "w")
-number = int(input("How many people do you want to add to this file?"))
-for i in range(number):
-    name = input("What is the name of person?")
-    phone = input("What is their phone number?")
-    email = input("What is their email?")
-    file.write(",".join([name, phone, email]) + '\n')
-file.close()
+def main():
+    number = int(input("How many people do you want to add to this file?"))
+    address_book = open("address.txt", "w")
+    for person in range(0, number):
+        name = input("What is the name of person?")
+        phone = input("What is their phone number?")
+        email = input("What is there email address?")
+        address_book.write(name + "," + phone + "," + email + "\n")
+    address_book.close()
+
+    
+main()
